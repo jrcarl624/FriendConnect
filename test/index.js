e69318b977feb30509c8a657a008042621856198
Joshua Carlson <61999256+minerj101@users.noreply.github.com>
2022-07-17 21:37:22 -0400

    Fixed restart loop with .env

--- test/index.js
+++ test/index.js
@@ -1,26 +1,21 @@
-import { Session } from "../dist/index.js";
-import auth from "prismarine-auth";
-const { Authflow, Titles } = auth;
+import { Session, XboxLive } from "../dist/index.js";
 
-const token = await new Authflow("TailvileMC", "./auth", {
-	authTitle: Titles.MinecraftNintendoSwitch,
-	deviceType: "Nintendo",
-}).getXboxToken();
+/**
+new Session({
+	hostName: "Tailvile.xyz",
+	worldName: "Fun Fact: Tailvile is almost 2 years old.",
+	version: "1.19.10",
+	protocol: 534,
+	players: 0,
+	maxPlayers: 20,
+	ip: "tailvile.xyz",
+	port: 19132,
+	log: true,
+	connectionType: 6,
+	keepVersionAndProtocolConstant: true,
+	autoFriending: true,
+});
+*/
+//console.log(new XboxLive(token).getAchievements());
 
-new Session(
-	{
-		hostName: "Tailvile.xyz",
-		worldName: "Fun Fact: Tailvile is almost 2 years old.",
-		version: "1.19.10",
-		protocol: 534,
-		players: 0,
-		maxPlayers: 20,
-		ip: "tailvile.xyz",
-		port: 19132,
-		log: true,
-		connectionType: 6,
-		keepVersionAndProtocolConstant: true,
-		autoFriending: true,
-	},
-	token
-);
+// get a value from a promise as a non promise
